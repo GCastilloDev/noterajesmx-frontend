62f3fb92bd02370109a67efeda054ece76e62ef6
tavoGeek <gcnctz@gmail.com>
2020-04-06 17:08:53 -0500

    CREACIÓN DEL COMPONENTE PARA CATEGORIAS
    * Se creo el componente CardCategoria.vue con el que se visualizaran las categorías.
    * Se creo la carpeta img dentro de assets, donde se copiaron los .svg que se ocuparan en las categorías.
    
    Queda pendiente la configuración del componente.

---
+++ src/components/CardCategoria.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-row dense>
+      <v-col cols="4">
+        <v-card color="#385F73" dark>
+          <v-card-title class="headline">Unlimited music now</v-card-title>
+
+          <v-card-subtitle
+            >Listen to your favorite artists and albums whenever and wherever,
+            online and offline.</v-card-subtitle
+          >
+
+          <v-card-actions>
+            <v-btn text>Listen Now</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CardCategoria",
+};
+</script>
+
+<style lang="css" scoped>
+    div{
+        margin-top: 1rem;
+    }
+</style>
---
+++ src/views/InicioView.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-container>    
+        <CardCategoria />
+    </v-container>
+    
+</template>
+
+<script>
+
+import CardCategoria from "../components/CardCategoria"
+    export default {
+        name: "Inicio",
+        components: {
+            CardCategoria
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+    div{
+        margin-top: -10px;
+    }
+</style>
