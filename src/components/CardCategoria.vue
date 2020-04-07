109d3940464ed106d139938284f7fbe258f02360
tavoGeek <gcnctz@gmail.com>
2020-04-06 23:50:07 -0500

    INSTALACIÓN DE VUEAXIOS
    * Se instalo VueAxios con el comando npm install --save axios vue-axios.
    * Se configuró VueAxios importando a axios y VueAxios en el main.js.
    * Se instancio VueAxios y axios en Vue para que Vue pueda usarlo.

--- src/components/CardCategoria.vue
+++ src/components/CardCategoria.vue
@@ -1,18 +1,17 @@
 <template>
   <div>
     <v-row dense>
-      <v-col cols="4">
-        <v-card color="#385F73" dark>
-          <v-card-title class="headline">Unlimited music now</v-card-title>
-
-          <v-card-subtitle
-            >Listen to your favorite artists and albums whenever and wherever,
-            online and offline.</v-card-subtitle
-          >
-
-          <v-card-actions>
-            <v-btn text>Listen Now</v-btn>
-          </v-card-actions>
+      <v-col cols="3" class="d-flex child-flex" v-for="(item, index) in items" :key="index">
+        <v-card color="#385F73" 
+            dark>
+                <v-img
+                  :src="item"
+                  aspect-ratio="1"
+                  width="100%"
+                  class=""
+                >
+                <v-card-title> Prueba </v-card-title>
+                </v-img>
         </v-card>
       </v-col>
     </v-row>
@@ -20,8 +19,33 @@
 </template>
 
 <script>
+import axios from "axios"
+
 export default {
   name: "CardCategoria",
+  data: () => ({
+      urlImg: "@/assets/img/categories/store_green.svg",
+      n: "store_green.svg",
+      items: [
+        {src: require('@/assets/img/categories/store_green.svg')},
+        {src: require('@/assets/img/categories/car_green.svg')},
+        {src: require('@/assets/img/categories/cleaning_green.svg')}
+      ],
+      apiRequest: ""
+  }),
+  methods: {
+      async api(){          
+          try {
+              let response = await axios.get("https://lucy-coatza.herokuapp.com/categories/");
+              console.log(response);
+          } catch (error) {
+              console.log(error);
+          }
+      }
+  },
+  created() {
+      this.api();
+  }
 };
 </script>
 
