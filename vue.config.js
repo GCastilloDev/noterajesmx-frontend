58a92d637157ddc88ec0f9dc03984a4403e2d411
tavoGeek <gcnctz@gmail.com>
2020-05-12 19:51:39 -0500

    GCN

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
+    chainWebpack: config => {
+        config.plugin('VuetifyLoaderPlugin').tap(args => [{
+            match(originalTag, { kebabTag, camelTag, path, component }) {
+                if (kebabTag.startsWith('core-')) {
+                    return [camelTag, `import ${camelTag} from '@/components/core/${camelTag.substring(4)}.vue'`]
+                }
+            }
+        }])
+    },
+    "transpileDependencies": [
+        "vuetify"
+    ]
 }
