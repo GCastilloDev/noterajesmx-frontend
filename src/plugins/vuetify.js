b06ba6780289e59db093c92b2a94bb551c89ab3d
tavoGeek <gcnctz@gmail.com>
2020-05-13 01:19:42 -0500

    GCN

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,30 +1,34 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import es from 'vuetify/es5/locale/es';
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-  theme: {
-      options: {
-        customProperties: true,
-      },
-    themes: {
-      light: {
-        verde: "#24cc89",
-        morado: '#71009F',
-        primary: '#24cc89',
-        secondary: '#424242',
-        accent: '#82B1FF',
-        error: '#FF5252',
-        info: '#2196F3',
-        success: '#4CAF50',
-        warning: '#FFC107'
-      },
+    theme: {
+        options: {
+            customProperties: true,
+        },
+        themes: {
+            light: {
+                verde: "#24cc89",
+                morado: '#71009F',
+                primary: '#24cc89',
+                secondary: '#424242',
+                accent: '#82B1FF',
+                error: '#FF5252',
+                info: '#2196F3',
+                success: '#4CAF50',
+                warning: '#FFC107'
+            },
+        },
     },
-  },
+    icons: {
+        iconfont: 'mdi', // default - only for display purposes
+    },
     lang: {
-      locales: { es },
-      current: 'es',
+        locales: { es },
+        current: 'es',
     },
 });
