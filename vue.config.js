ac87a3cb7ea1b6a862970576c9d4d37a9a285e99
tavoGeek <gcnctz@gmail.com>
2020-05-12 21:16:47 -0500

    GCN

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
+const PreloadWebpackPlugin = require('preload-webpack-plugin');
+
 module.exports = {
     "transpileDependencies": [
         "vuetify"
-    ]
+    ],
+    configureWebpack: {
+        plugins: [
+            new PreloadWebpackPlugin()
+        ]
+    }
 }
