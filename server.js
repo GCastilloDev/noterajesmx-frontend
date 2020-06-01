6835cdf36bdbf46c0fe2ad271ff78d6f289301e4
tavoGeek <gcnctz@gmail.com>
2020-05-31 19:57:25 -0500

    GCN

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express')
-    //const serveStatic = require('serve-static')
-var expressStaticGzip = require("express-static-gzip");
+const serveStatic = require('serve-static')
+//var expressStaticGzip = require("express-static-gzip");
 const path = require('path')
 
 const app = express()
 
 //here we are configuring dist to serve app files
-app.use('/', expressStaticGzip(path.join(__dirname, '/dist')))
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function(req, res) {
